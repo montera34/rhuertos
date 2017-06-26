392bfa8c340959c91192385cecab750698889954
skotperez <voraginebramante@gmail.com>
2017-06-26 12:29:24 +0200

    Fixes header to top when scroll down

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -1,5 +1,20 @@
 (function($) {
 
-	// all Javascript code goes here
+	// fix header when scroll down
+	var win = $(window);
+	var offset = Number($('#ante').height());
+	var offsetFixed = Number($('#pre').height());
+
+	win.scroll(function () {
+		if ( win.scrollTop() > offset ) {
+			$('#pre').addClass('navbar-fixed-top');
+			$('#pre-menu').addClass('navbar-fixed-top');
+			$('#pre-menu').css({top: offsetFixed+'px'});
+		} else if ( win.scrollTop() < offset && $('#pre').hasClass('navbar-fixed-top') ) {
+			$('#pre').removeClass('navbar-fixed-top');
+			$('#pre-menu').removeClass('navbar-fixed-top');
+			$('#pre-menu').css({top: '0'});
+		}
+	});
 
 })(jQuery);
