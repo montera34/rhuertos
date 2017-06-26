34187b00bddf210f55c2c058f92baf989991afe7
skotperez <voraginebramante@gmail.com>
2017-06-26 23:08:25 +0200

    Default page template

--- content-page.php
+++ content-page.php
@@ -4,14 +4,21 @@
  *
  * @package _mbbasetheme
  */
+
+$img_size = 'post-thumbnail';
+if ( has_post_thumbnail() ) {
+	$loop_image = '<aside class="col-sm-4"><figure class="'.$loop_prefix.'-img">'.get_the_post_thumbnail($post->ID,$img_size,array('class' => 'img-responsive')).'</figure></aside>';
+} else {
+	$loop_image = "";
+}
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
+<article id="post-<?php the_ID(); ?>" <?php post_class('block container'); ?>>
+	<header class="row">
+		<?php the_title( '<h1 class="col-sm-12 entry-title">', '</h1>' ); ?>
+	</header>
+	<div class="row entry-content">
+		<div class="col-sm-8">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
@@ -19,6 +26,8 @@
 				'after'  => '</div>',
 			) );
 		?>
+		</div>
+		<?php echo $loop_image; ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-footer">
 		<?php edit_post_link( __( 'Edit', '_mbbasetheme' ), '<span class="edit-link">', '</span>' ); ?>
--- page.php
+++ page.php
@@ -12,24 +12,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<?php
+	// SLIDESHOW
+	////
+	get_template_part( 'content', 'carousel' );
 
-				<?php get_template_part( 'content', 'page' ); ?>
+	while ( have_posts() ) : the_post();
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+		get_template_part( 'content', 'page' );
 
-			<?php endwhile; // end of the loop. ?>
+		// If comments are open or we have at least one comment, load up the comment template
+		if ( comments_open() || '0' != get_comments_number() ) :
+			comments_template();
+		endif;
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	endwhile; // end of the loop. ?>
 
-<?php get_sidebar(); ?>
+</main><!-- #main -->
+
 <?php get_footer(); ?>
